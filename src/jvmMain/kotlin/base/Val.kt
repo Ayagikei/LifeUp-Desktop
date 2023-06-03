d1434704608914892f01953e75081a3be0bad4eb
AyagiKei <kei.ayagi@gmail.com>
2023-06-03 23:13:08 +0800

    feat: update versions

--- src/jvmMain/kotlin/base/Val.kt
+++ src/jvmMain/kotlin/base/Val.kt
@@ -2,9 +2,9 @@ package base
 
 object Val {
     // FIXME: get version from gradle
-    val version: String = "1.0.2"
+    val version: String = "1.1.0"
 
-    val targetLifeUpCloudVersion = "1.1.2+"
+    val targetLifeUpCloudVersion = "1.2.0+"
 
-    val targetLifeUpAndroidVersion = "1.91.0+"
+    val targetLifeUpAndroidVersion = "1.91.3+"
 }
